e3b11acb2411e38b677b7d74d39db70a406680fc
Ian Littman <ian@cloudyhills.com>
2015-02-15 02:16:07 -0600

    Extract parameter tests for FilterFactory methods
    ...because Scrutinizer

--- src/FilterFactory.php
+++ src/FilterFactory.php
@@ -23,6 +23,20 @@ class FilterFactory
      */
     public static function monthAndDay($month, $day)
     {
+        static::checkMonthAndDayArgs($month, $day);
+
+        return function(\DateTimeInterface $dt) use ($month, $day) {
+            return (int) $dt->format('m') === $month && (int) $dt->format('d') === $day;
+        };
+    }
+
+    /**
+     * Does type and bounds checks for monthAndDay()
+     *
+     * @param $month
+     * @param $day
+     */
+    protected static function checkMonthAndDayArgs($month, $day) {
         if (!is_int($month) || !is_int($day)) {
             throw new \InvalidArgumentException('$month and $day must be integers');
         }
@@ -38,10 +52,6 @@ class FilterFactory
         if ((in_array($month, [4, 6, 9, 11]) && $day > 30) || ((int) $month === 2 && $day > 29)) {
             throw new \OutOfBoundsException('Day ' . $day . ' does not exist in month ' . $month);
         }
-
-        return function(\DateTimeInterface $dt) use ($month, $day) {
-            return (int) $dt->format('m') === $month && (int) $dt->format('d') === $day;
-        };
     }
 
     /**
@@ -56,6 +66,29 @@ class FilterFactory
      * @throws \OutOfBoundsException|\InvalidArgumentException
      */
     public static function nthDayOfWeekOfMonth($n, $day_of_week, $month) {
+        static::checkNthDayArgs($n, $day_of_week, $month);
+
+        $lowerBound = ($n - 1) * 7;
+        $upperBound = $n * 7;
+
+        return function(\DateTimeInterface $dt) use ($month, $day_of_week, $lowerBound, $upperBound) {
+            if ((int) $dt->format('m') !== $month) {
+                return false;
+            }
+
+            return ((int) $dt->format('w')) === $day_of_week &&
+                    $dt->format('d') > $lowerBound && $dt->format('d') <= $upperBound;
+        };
+    }
+
+    /**
+     * Does type and bounds checks for nThDayOfWeekOfMonth()
+     *
+     * @param $n
+     * @param $day_of_week
+     * @param $month
+     */
+    protected static function checkNthDayArgs($n, $day_of_week, $month) {
         if (!is_int($n) || !is_int($day_of_week) || !is_int($month)) {
             throw new \InvalidArgumentException('$n, $day_of_week and $month must be integers');
         }
@@ -71,17 +104,5 @@ class FilterFactory
         if ($month < 1 || $month > 12) {
             throw new \OutOfBoundsException('$month must be 1-12');
         }
-
-        $lowerBound = ($n - 1) * 7;
-        $upperBound = $n * 7;
-
-        return function(\DateTimeInterface $dt) use ($month, $day_of_week, $lowerBound, $upperBound) {
-            if ((int) $dt->format('m') !== $month) {
-                return false;
-            }
-
-            return ((int) $dt->format('w')) === $day_of_week &&
-                    $dt->format('d') > $lowerBound && $dt->format('d') <= $upperBound;
-        };
     }
 }
